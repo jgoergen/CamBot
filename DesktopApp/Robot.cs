18b66b02d8066c7fdeae473a8a1bf451c70def32
Jeff Goergen <jeffg@wambatech.com>
2017-11-17 16:50:59 -0500

    Add files via upload

---
+++ DesktopApp/Robot.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace VideoSurveilance
+{
+    public class Robot
+    {
+        public bool Ready = false;
+        public int R = 0; // TODO: turn this into get / set instead
+        public int G = 0; // TODO: turn this into get / set instead
+        public int B = 0; // TODO: turn this into get / set instead
+        public int LEDChangeSpeed = 0; // TODO: turn this into get / set instead
+        public int Pan = 0; // TODO: turn this into get / set instead
+        public int Tilt = 0; // TODO: turn this into get / set instead
+        public int ServoMoveSpeed = 0; // TODO: turn this into get / set instead
+        public int HomePanValue = 0;
+        public int HomeTiltValue = 0;
+
+        private string tString = string.Empty;
+        private byte serialStringTerminator = 0x4;
+        private SerialPort serialPort = new SerialPort();
+
+        public Robot(int servoMoveSpeed, int ledChangeSpeed, int homePanValue, int homeTiltValue)
+        {
+            LEDChangeSpeed = ledChangeSpeed;
+            ServoMoveSpeed = servoMoveSpeed;
+            HomePanValue = homePanValue;
+            HomeTiltValue = homeTiltValue;
+
+            OpenSerialPort();
+            if (Ready)
+                Home();
+        }
+
+        public void Home()
+        {
+            SetLED(0, 0, 255);
+            Set(HomePanValue, HomeTiltValue);
+            Debug.WriteLine("Homing");
+        }
+
+        public void Move(int? xDist, int? yDist)
+        {
+            if (xDist == null)
+            {
+                xDist = 0;
+            }
+            else
+            {
+                Pan += xDist.Value;
+            }
+
+            if (yDist == null)
+            {
+                yDist = 0;
+            }
+            else
+            {
+                Tilt += yDist.Value;
+            }
+
+            if (xDist < 0)
+            {
+                Debug.WriteLine("Move left " + Math.Abs(xDist.Value));
+                serialPort.WriteLine("<panleft:" + Math.Abs(xDist.Value) + ">");
+                serialPort.WriteLine("");
+            }
+            else if (xDist > 0)
+            {
+                Debug.WriteLine("Move right " + xDist);
+                serialPort.WriteLine("<panright:" + xDist + ">");
+                serialPort.WriteLine("");
+            }
+
+            if (yDist < 0)
+            {
+                Debug.WriteLine("Move up " + Math.Abs(yDist.Value));
+                serialPort.WriteLine("<tiltup:" + Math.Abs(yDist.Value) + ">");
+                serialPort.WriteLine("");
+            }
+            else if (yDist > 0)
+            {
+                Debug.WriteLine("Move down " + yDist);
+                serialPort.WriteLine("<tiltdown:" + yDist + ">");
+                serialPort.WriteLine("");
+            }
+        }
+
+        public void Set(int? pan, int? tilt)
+        {
+            if (pan == null)
+                pan = Pan;
+            else
+                Pan = pan.Value;
+
+            if (tilt == null)
+                tilt = Tilt;
+            else
+                Tilt = tilt.Value;
+
+            Debug.WriteLine("Setting pan " + pan + ", tilt " + tilt);
+            serialPort.WriteLine("<panset:" + pan + ">");
+            serialPort.WriteLine("<tiltset:" + tilt + ">");
+        }
+
+        public void SetLED(int? r, int? g, int? b)
+        {
+            // keel local values up to date and apply local value if param is null
+
+            if (r == null)
+                r = R;
+            else
+                R = r.Value;
+
+            if (g == null)
+                g = G;
+            else
+                G = g.Value;
+
+            if (b == null)
+                b = B;
+            else
+                B = b.Value;
+
+            // send commands
+            
+            serialPort.WriteLine("<setr:" + r + ">");
+            serialPort.WriteLine("<setg:" + g + ">");
+            serialPort.WriteLine("<setb:" + b + ">");
+        }
+
+        public void SetServoMoveSpeed(int speed)
+        {
+            serialPort.WriteLine("<setservospeed:" + speed + "> ");
+        }
+
+        public void SetLEDChangeSpeed(int speed)
+        {
+            serialPort.WriteLine("<setledspeed:2>");
+        }
+
+        public void OpenSerialPort()
+        {
+            string portNum = "";
+            serialPort.Close();
+
+            foreach (string ports in SerialPort.GetPortNames())
+            {
+                portNum = ports.ToString();
+                Debug.WriteLine("Trying port: " + portNum);
+
+                try
+                {
+                    serialPort.Close();
+                    serialPort.PortName = (portNum);
+                    serialPort.BaudRate = 9600;
+                    serialPort.DataBits = 8;
+                    serialPort.Parity = Parity.None;
+                    serialPort.StopBits = StopBits.One;
+                    serialPort.Handshake = Handshake.None;
+                    serialPort.Encoding = System.Text.Encoding.Default;
+                    serialPort.Open();
+                    serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+                    SetLEDChangeSpeed(LEDChangeSpeed);
+                    SetServoMoveSpeed(ServoMoveSpeed);
+                    serialPort.WriteLine("");
+                    Ready = true;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Unable to use port.");
+                    Debug.WriteLine(e.Message);
+                }
+            }
+        }
+
+        void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] buffer = new byte[serialPort.ReadBufferSize];
+            int bytesRead = serialPort.Read(buffer, 0, buffer.Length);
+            tString += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            if (tString.IndexOf((char)serialStringTerminator) > -1)
+            {
+                string workingString = tString.Substring(0, tString.IndexOf((char)serialStringTerminator));
+                tString = tString.Substring(tString.IndexOf((char)serialStringTerminator));
+                Debug.WriteLine("Serial Data");
+                Debug.WriteLine(workingString);
+            }
+        }
+    }
+}
--- DesktopApp/VideoSurveilance.cs
+++ DesktopApp/VideoSurveilance.cs
@@ -24,237 +24,118 @@ namespace VideoSurveilance
     public partial class VideoSurveilance : Form
     {
         // SETTINGS ////////////////////////////////////////////////
-        private int SUBSTRACTION_HISTORY = 20; // the number of frames to use when normalizing out 'noise'
-        private int SUBTRACTION_THRESHOLD = 30; // the amount of pixel difference to ignore when comparing sets of frames
+        private int SUBTRACTION_HISTORY = 20; // the number of frames to use when normalizing out 'noise'
+        private int SUBTRACTION_THRESHOLD = 20; // the amount of pixel difference to ignore when comparing sets of frames
         private int FRAME_BLUR_STRENGTH = 101; // this MUST be an odd number
         private int LARGEST_DETECTION_HEIGHT_SIZE_DIVISOR = 2;
         private int LARGEST_DETECTION_WIDTH_SIZE_DIVISOR = 2;
-        private int SMALLEST_DETECTION_HEIGHT_SIZE_DIVISOR = 4;
-        private int SMALLEST_DETECTION_WIDTH_SIZE_DIVISOR = 5;
-        private long MS_MOVE_WAIT = 500; // how long to wait before looking moving
-        private long MS_PAUSE_DETECT_AFTER_MOVE = 500; // how long to wait after a move for the detection to 'settle'
-        private int MOVE_ADJUST = 10; // the lower this number the further the servos move towards percieved movement
+        private int SMALLEST_DETECTION_HEIGHT_SIZE_DIVISOR = 6;
+        private int SMALLEST_DETECTION_WIDTH_SIZE_DIVISOR = 10;
+        private long DEFAULT_MOVE_WAIT = 200; // how long to wait before looking moving
+        private int MOVE_ADJUST = 30; // the lower this number the further the servos move towards percieved movement
+        private long MS_WAIT_BEFORE_HOMING = 10 * 1000; // the amount of time the camera waits before returning to home postiion. movement resets wait.
+        private int HOME_PAN = 40;
+        private int HOME_TILT = 40;
+        private int SERVO_MOVE_SPEED = 20;
+        private int LED_CHANGE_SPEED = 2;
+        private int MOVE_DIST_WAIT_MODIFIER = 40; // this is multiplied by the largest move distance and added to the natural wait time after a move.
+        private int PAUSE_AFTER_HOMING = 400;
         // END SETTINGS ////////////////////////////////////////////
 
-        private static VideoCapture _cameraCapture;
+        private Robot robot;
+        private CameraTracking camera;
+
         private Stopwatch sw = Stopwatch.StartNew();
         private long lastTimeMoved = 0;
-        private static BackgroundSubtractor _fgDetector;
-        private static Emgu.CV.Cvb.CvBlobDetector _blobDetector;
-        private static Emgu.CV.Cvb.CvTracks _tracker;
-        private SerialPort _serialPort = new SerialPort();
-        private double centerX = 0;
-        private double centerY = 0;
-        private bool moving = false;
-        private string tString = string.Empty;
-        private byte _terminator = 0x4;
+        private long next_home_time = 0;
+        private long moveWait = 0;
 
         public VideoSurveilance()
         {
             InitializeComponent();
-            OpenSerialPort();
-            Run();
-        }
 
-        public void OpenSerialPort()
-        {
-            string portNum = "";
-            _serialPort.Close();
+            robot = 
+                new Robot(
+                    SERVO_MOVE_SPEED, 
+                    LED_CHANGE_SPEED, 
+                    HOME_PAN, 
+                    HOME_TILT);
 
-            foreach (string ports in SerialPort.GetPortNames())
+            camera =
+                new CameraTracking(
+                    SUBTRACTION_HISTORY,
+                    SUBTRACTION_THRESHOLD,
+                    FRAME_BLUR_STRENGTH,
+                    LARGEST_DETECTION_HEIGHT_SIZE_DIVISOR,
+                    LARGEST_DETECTION_WIDTH_SIZE_DIVISOR,
+                    SMALLEST_DETECTION_HEIGHT_SIZE_DIVISOR,
+                    SMALLEST_DETECTION_WIDTH_SIZE_DIVISOR);
+
+            if (!camera.Ready)
             {
-                portNum = ports.ToString();
-                Debug.WriteLine("Trying port: " + portNum);
-
-                try
-                {
-                    _serialPort.PortName = (portNum);
-                    _serialPort.BaudRate = 9600;
-                    _serialPort.DataBits = 8;
-                    _serialPort.Parity = Parity.None;
-                    _serialPort.StopBits = StopBits.One;
-                    _serialPort.Handshake = Handshake.None;
-                    _serialPort.Encoding = System.Text.Encoding.Default;
-                    _serialPort.Open();
-                    _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataReceived);
-                    _serialPort.WriteLine("<setledspeed:2>");
-                    _serialPort.WriteLine("<setr:0>");
-                    _serialPort.WriteLine("<setg:0>");
-                    _serialPort.WriteLine("<setb:0>");
-                    _serialPort.WriteLine("<panset:50>");
-                    _serialPort.WriteLine("<tiltset:30>");
-                    _serialPort.WriteLine("");
-                }
-                catch (Exception e)
-                {
-                    Debug.WriteLine("Unable to use port.");
-                    Debug.WriteLine(e.Message);
-                }
-            }
-
-        }
-
-        void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            byte[] buffer = new byte[_serialPort.ReadBufferSize];
-            int bytesRead = _serialPort.Read(buffer, 0, buffer.Length);
-            tString += Encoding.ASCII.GetString(buffer, 0, bytesRead);
-
-            if (tString.IndexOf((char)_terminator) > -1)
-            {
-                string workingString = tString.Substring(0, tString.IndexOf((char)_terminator));
-                tString = tString.Substring(tString.IndexOf((char)_terminator));
-                Debug.WriteLine("Serial Data");
-                Debug.WriteLine(workingString);
-            }
-        }
-
-        void Run()
-        {
-            try
-            {
-                _cameraCapture = new VideoCapture();
-
-                // I had to set this by hand to match our camera as opencv doesn't always pull these properties correctly and sometimes shows funky frames or nothing at all
-                _cameraCapture.SetCaptureProperty(CapProp.FrameWidth, 1600);
-                _cameraCapture.SetCaptureProperty(CapProp.FrameHeight, 1200);
-                _cameraCapture.SetCaptureProperty(CapProp.FourCC, Emgu.CV.VideoWriter.Fourcc('Y', 'U', 'Y', '2'));
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
+                Debug.WriteLine("Could not find Camera.");
                 return;
             }
 
-            _fgDetector = new Emgu.CV.VideoSurveillance.BackgroundSubtractorMOG2(SUBSTRACTION_HISTORY, SUBTRACTION_THRESHOLD);
-            _blobDetector = new CvBlobDetector();
-            _tracker = new CvTracks();
-            Application.Idle += ProcessFrame;
-        }
-
-        void ProcessFrame(object sender, EventArgs e)
-        {
-            // capture frame
-            Mat frame = _cameraCapture.QuerySmallFrame();
-            Mat smoothedFrame = new Mat();
-            CvInvoke.GaussianBlur(frame, smoothedFrame, new Size(FRAME_BLUR_STRENGTH, FRAME_BLUR_STRENGTH), 1); //filter out noises
-
-            // get mask for preview
-            Mat forgroundMask = new Mat();
-            _fgDetector.Apply(smoothedFrame, forgroundMask);
-
-            // get detection 'blobs' or regions
-            CvBlobs blobs = new CvBlobs();
-            _blobDetector.Detect(forgroundMask.ToImage<Gray, byte>(), blobs);
-            blobs.FilterByArea(100, int.MaxValue);
-
-            float scale = (frame.Width + frame.Width) / 2.0f;
-            _tracker.Update(blobs, 0.01 * scale, 5, 5);
-
-            double largestW = 0;
-            double largestH = 0;
-
-            foreach (var pair in _tracker)
+            if (!camera.Ready)
             {
-                CvTrack b = pair.Value;
-
-                // limit the largest and smallest size boxes we care about.
-                if (b.BoundingBox.Width < (frame.Width / SMALLEST_DETECTION_WIDTH_SIZE_DIVISOR) ||
-                    b.BoundingBox.Height < (frame.Height / SMALLEST_DETECTION_HEIGHT_SIZE_DIVISOR) ||
-                    (b.BoundingBox.Width > (frame.Width / LARGEST_DETECTION_WIDTH_SIZE_DIVISOR) &&
-                    b.BoundingBox.Height > (frame.Height / LARGEST_DETECTION_HEIGHT_SIZE_DIVISOR)))
-                    continue;
-
-                // keep track of the largest regions as we only care to track the largest
-                if (b.BoundingBox.Width > largestW)
-                {
-                    largestW = b.BoundingBox.Width;
-                    largestH = b.BoundingBox.Height;
-                    centerX = b.Centroid.X;
-                    centerY = b.Centroid.Y;
-                    CvInvoke.Rectangle(frame, b.BoundingBox, new MCvScalar(255.0, 255.0, 255.0), 20);
-                    CvInvoke.PutText(frame, b.Id.ToString(), new Point((int)Math.Round(b.Centroid.X), (int)Math.Round(b.Centroid.Y)), FontFace.HersheyPlain, 1.0, new MCvScalar(255.0, 255.0, 255.0));
-                }
-                else
-                {
-                    CvInvoke.Rectangle(frame, b.BoundingBox, new MCvScalar(255.0, 255.0, 255.0), 1);
-                    CvInvoke.PutText(frame, b.Id.ToString(), new Point((int)Math.Round(b.Centroid.X), (int)Math.Round(b.Centroid.Y)), FontFace.HersheyPlain, 1.0, new MCvScalar(255.0, 255.0, 255.0));
-                }
+                Debug.WriteLine("Could not find Robot.");
+                return;
             }
 
+            Application.Idle += ProcessFrame;
+        }
+        
+        void ProcessFrame(object sender, EventArgs e)
+        {
+            var frameData = camera.Update();
+
             // display frames onscreen
-            imageBox1.Image = frame;
-            imageBox2.Image = forgroundMask;
+            imageBox1.Image = frameData.Frame;
+            imageBox2.Image = frameData.Mask;
 
             // if enough time has elapsed, deal with potential movement
-            if (sw.ElapsedMilliseconds > lastTimeMoved + MS_MOVE_WAIT)
+            if (sw.ElapsedMilliseconds > lastTimeMoved + moveWait)
             {
-                Debug.WriteLine(frame.Width + " " + frame.Height + " " + centerX + " " + centerY);
                 lastTimeMoved = sw.ElapsedMilliseconds;
+                moveWait = DEFAULT_MOVE_WAIT;
 
-                // did we just move? if so, skip movement this time around.
-                if (moving)
-                {
-                    moving = false;
-                    // reset wait time before next move
-                    MS_MOVE_WAIT -= MS_PAUSE_DETECT_AFTER_MOVE;
-                    centerX = 0;
-                    centerY = 0;
-                }
+                var subjectData =
+                    camera.FindSubjects();
 
-                // if there is no movement, fade back to 0,0,0
-                if (centerX == 0 && centerY == 0)
+                if (!subjectData.FoundSubject)
                 {
-                    _serialPort.WriteLine("<setr:0>");
-                    _serialPort.WriteLine("<setg:0>");
-                    _serialPort.WriteLine("<setb:0>");
+                    
+                    if (sw.ElapsedMilliseconds > next_home_time)
+                    {
+                        robot.Home();
+                        moveWait += PAUSE_AFTER_HOMING;
+                        next_home_time = (lastTimeMoved + MS_WAIT_BEFORE_HOMING) * 10 + moveWait;                        
+                    }
+                    else
+                    {
+                        robot.SetLED(0, 0, 0);
+                    }
+                    
                     return;
                 }
-                else
-                {
-                    _serialPort.WriteLine("<setg:255>");
-                }
 
-                var xDist = (((frame.Width / 2) - centerX) / MOVE_ADJUST);
-                var yDist = (((frame.Height / 2) - centerY) / MOVE_ADJUST) * -1;
+                robot.SetLED(0, 255, 0);
+                next_home_time = lastTimeMoved + MS_WAIT_BEFORE_HOMING;
 
-                if (xDist < 0)
-                {
-                    Debug.WriteLine("Move left " + Math.Abs(xDist));
-                    _serialPort.WriteLine("<panleft:" + Math.Abs(xDist) + ">");
-                    _serialPort.WriteLine("");
-                    moving = true;
-                }
-                else if (xDist > 0)
-                {
-                    Debug.WriteLine("Move right " + xDist);
-                    _serialPort.WriteLine("<panright:" + xDist + ">");
-                    _serialPort.WriteLine("");
-                    moving = true;
-                }
-
-                if (yDist < 0)
-                {
-                    Debug.WriteLine("Move up " + Math.Abs(yDist));
-                    _serialPort.WriteLine("<tiltup:" + Math.Abs(yDist)  + ">");
-                    _serialPort.WriteLine("");
-                    moving = true;
-                }
-                else if (yDist > 0)
-                {
-                    Debug.WriteLine("Move down " + yDist);
-                    _serialPort.WriteLine("<tiltdown:" + yDist  + ">");
-                    _serialPort.WriteLine("");
-                    moving = true;
-                }
-
-                // clear out values now that we've moved.
-                centerX = 0;
-                centerY = 0;
+                int xDist = (int)Math.Floor(((camera.FrameWidth / 2) - subjectData.CenterX) / MOVE_ADJUST);
+                int yDist = (int)Math.Floor(((camera.FrameHeight / 2) - subjectData.CenterY) / MOVE_ADJUST) * -1;
+                robot.Move(xDist, yDist);
 
                 // if we did actually move add alittle extra time onto the wait before movin again
-                if (moving)
-                    MS_MOVE_WAIT += MS_PAUSE_DETECT_AFTER_MOVE;
+                moveWait +=
+                    (long)(Math.Abs(Math.Max(xDist, yDist) * MOVE_DIST_WAIT_MODIFIER));
+
+                Debug.WriteLine("Waiting " + moveWait);
+                Debug.WriteLine("");
+            } else
+            {
+                Debug.Write(".");
             }
         }
     }
