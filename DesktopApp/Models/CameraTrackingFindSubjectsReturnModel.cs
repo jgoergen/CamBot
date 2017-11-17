3786c0f410430694a1e8ae92b6e3f3ad00fb69e9
Jeff Goergen <jeffg@wambatech.com>
2017-11-17 16:51:24 -0500

    Add files via upload

---
+++ DesktopApp/Models/CameraTrackingFindSubjectsReturnModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace VideoSurveilance
+{
+    public class CameraTrackingFindSubjectsReturnModel
+    {
+        public double CenterX = 0;
+        public double CenterY = 0;
+        public Rectangle BoundingBox;
+        public bool FoundSubject;
+    }
+}
---
+++ DesktopApp/Models/CameraTrackingUpdateReturnModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace VideoSurveilance
+{
+    public class CameraTrackingUpdateReturnModel
+    {
+        public Emgu.CV.Mat Frame;
+        public Emgu.CV.Mat Mask;
+    }
+}
